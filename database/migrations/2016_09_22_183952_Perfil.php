ddc1f78ab7320900f2f26565fceb123f7ccfafe1
Jayr Alencar <jayralencarpereira@gmail.com>
2016-09-22 15:45:43 -0300

    Migrations

--- database/migrations/2016_09_22_183952_Perfil.php
+++ database/migrations/2016_09_22_183952_Perfil.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Perfil extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('perfil', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->char('nome',200);
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('perfil');
     }
 }
--- database/migrations/2016_09_22_184032_Usuario.php
+++ database/migrations/2016_09_22_184032_Usuario.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class Usuario extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index();
-            $table->timestamp('created_at');
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_perfil');
+            $table->char('nome',200);
+            $table->char('email',200);
+            $table->timestamps();
         });
     }
 
@@ -26,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('password_resets');
+        Schema::drop('usuario');
     }
 }
